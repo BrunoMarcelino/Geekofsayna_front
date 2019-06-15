444c1c0e5bedb1ddc90c545b16e9646b93d45ffd
RakotoarimalalaNantenaina <naynah344@gmail.com>
2019-06-16 06:43:36 +1000

    modifier nantenaina

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,13 +1,137 @@
 import React from 'react';
+import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBFormInline, MDBDropdown, MDBDropdownToggle, MDBDropdownMenu,  MDBDropdownItem, MDBContainer, MDBIcon } from "mdbreact";
+import { MDBCarousel, MDBCarouselCaption, MDBCarouselInner, MDBCarouselItem, MDBView, MDBMask} from "mdbreact";
 
-class Login extends React.Component {
+class Home extends React.Component {
+    state = {
+        collapseID: ""
+      };
+    
+      toggleCollapse = collapseID => () =>
+        this.setState(prevState => ({
+          collapseID: prevState.collapseID !== collapseID ? collapseID : ""
+        }));
+
+
+
     render() {
         return (
             <div>
-           Accueil
-        </div>
+          <MDBContainer className="pb-5">
+          <MDBNavbar id="Totalhome"
+              color="bg-danger"
+             style={{ Bacgroundcolor: "blue" }}
+              dark
+              expand="md"
+              scrolling
+            >
+              <MDBContainer>
+                <MDBNavbarBrand>
+                  <strong className="white-text">LOGO</strong>
+                </MDBNavbarBrand>
+                <MDBNavbarToggler
+                  onClick={this.toggleCollapse("navbarCollapse")}
+                />
+                <MDBCollapse
+                  id="navbarCollapse"
+                  isOpen={this.state.collapseID}
+                  navbar
+                >
+                  <MDBNavbarNav left>
+                    <MDBNavItem active>
+                      <MDBNavLink to="#!">Accueil</MDBNavLink>
+                    </MDBNavItem>
+                    <MDBNavItem active>
+                      <MDBNavLink to="#!">A propos</MDBNavLink>
+                    </MDBNavItem>
+                    <MDBNavItem active>
+                      <MDBNavLink to="#!">Contact</MDBNavLink>
+                    </MDBNavItem>
+                  </MDBNavbarNav>
+                  <MDBNavbarNav right>
+                    <MDBNavItem active>
+                        <MDBNavLink to="">Login</MDBNavLink>
+                    </MDBNavItem>
+                    <MDBNavItem active>
+                        <MDBNavLink to="#!">Register</MDBNavLink>
+                    </MDBNavItem>
+                  </MDBNavbarNav>
+                </MDBCollapse>
+              </MDBContainer>
+            </MDBNavbar>
+            <h4 className="mt-5 mb-3">With Captions</h4>
+      <MDBCarousel
+        activeItem={1}
+        length={4}
+        showControls={true}
+        showIndicators={true}
+        className="z-depth-1"
+      >
+        <MDBCarouselInner>
+          <MDBCarouselItem itemId="1">
+            <MDBView>
+              <img
+                className="d-block w-100"
+                src="https://mdbootstrap.com/img/Photos/Slides/img%20(68).jpg"
+                alt="First slide"
+              />
+              <MDBMask overlay="black-light" />
+            </MDBView>
+            <MDBCarouselCaption>
+              <h3 className="h3-responsive">Light mask</h3>
+              <p>First text</p>
+            </MDBCarouselCaption>
+          </MDBCarouselItem>
+          <MDBCarouselItem itemId="2">
+            <MDBView>
+              <img
+                className="d-block w-100"
+                src="../images/cerveau.png"
+                alt="Second slide"
+              />
+              <MDBMask overlay="black-strong" />
+            </MDBView>
+            <MDBCarouselCaption>
+              <h3 className="h3-responsive">Strong mask</h3>
+              <p>Second text</p>
+            </MDBCarouselCaption>
+          </MDBCarouselItem>
+          <MDBCarouselItem itemId="3">
+            <MDBView>
+              <img
+                className="d-block w-100"
+                src="../images/entendu.png"
+                alt="Third slide"
+              />
+              <MDBMask overlay="black-slight" />
+            </MDBView>
+            <MDBCarouselCaption>
+              <h3 className="h3-responsive">Slight mask</h3>
+              <p>Third text</p>
+            </MDBCarouselCaption>
+          </MDBCarouselItem>
+          <MDBCarouselItem itemId="4">
+            <MDBView>
+              <img
+                className="d-block w-100"
+                src="../images/main.png"
+                alt="Mattonit's item"
+              />
+              <MDBMask overlay="black-light" />
+            </MDBView>
+            <MDBCarouselCaption>
+              <h3 className="h3-responsive">Sopot Beach</h3>
+              <p>Taken june 21th by @mattonit</p>
+            </MDBCarouselCaption>
+          </MDBCarouselItem>
+        </MDBCarouselInner>
+      </MDBCarousel>
+
+     <hr/>
+          </MDBContainer>
+            </div>
         )
     }
 }
 
-export default Login;
+export default Home;

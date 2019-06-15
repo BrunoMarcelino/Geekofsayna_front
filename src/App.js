2e1ffa76624001bc7c4d6a7c9a0ee13a09980da3
RakotoarimalalaNantenaina <naynah344@gmail.com>
2019-06-16 04:12:57 +1000

    2eme modification

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Accueil from './components/home'
 import Login from './components/login'
@@ -7,12 +8,12 @@ import Register from './components/register'
 
 function App() {
   return (
-    <div className="App">
+    <div className="">
       <Router>
         <Route exact path="/" component={Accueil} />
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
-        </Router>
+      </Router>
     </div>
   );
 }

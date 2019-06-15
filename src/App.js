34604a909ca80704a75598e8e6686d1e85314b3f
RakotoarimalalaNantenaina <naynah344@gmail.com>
2019-06-16 03:07:49 +1000

    modifier 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import './App.css';
-import './components/materialize.css';
-import './components/style.css';
-import './components/custom.css'
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Accueil from './components/home'
 import Login from './components/login'
 import Register from './components/register'
 
@@ -11,12 +9,9 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Route exact path="/" component={Login} />
+        <Route exact path="/" component={Accueil} />
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
-        <p>
-         Geek-of-sayna
-        </p>
         </Router>
     </div>
   );
---
+++ src/components/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './login.css';
+
+class Login extends React.Component {
+    render() {
+        return (
+            <div>
+           Accueil
+        </div>
+        )
+    }
+}
+
+export default Login;

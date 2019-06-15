d5c1abe6cfb53dc5cd196a4b7d9faa9e7f0d6cd7
RakotoarimalalaNantenaina <naynah344@gmail.com>
2019-06-16 01:17:44 +1000

    modification

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import logosayna from './images/logo.png';
 import './App.css';
+import './components/materialize.css';
+import './components/style.css';
+import './components/custom.css'
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from './components/login'
+import Register from './components/register'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logosayna} className="App-logo" alt="logo" />
+      <Router>
+        <Route exact path="/" component={Login} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
         <p>
          Geek-of-sayna
         </p>
-      </header>
+        </Router>
     </div>
   );
 }

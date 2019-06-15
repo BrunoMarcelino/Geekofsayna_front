a29a0f9cce811c59cf9958b5b015d1d2d5351db1
RakotoarimalalaNantenaina <naynah344@gmail.com>
2019-06-16 04:12:57 +1000

    2eme modification

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import './login.css';
+import './register.css'
 
 class Login extends React.Component {
     render() {
         return (
-            <div className="container-fluid">
+            <div id="totalregister">
+            <div className="container">
             <div className="row">
             <div className="col-md-3"></div>
                 <div className="col-md-6">
@@ -29,14 +30,14 @@ class Login extends React.Component {
                                         </div>
                                     </form>
                                 </div>
-
-
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="col-md-3"></div>
             </div>
+        </div>
+
         </div>
         )
     }
